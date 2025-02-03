276d572e99d80848831bb9ad7d0fba6e3f8f3599
ivan perkov <ivan1996perkov@gmail.com>
2025-02-03 14:47:03 +0200

    finishing homework

--- exersice_2.py
+++ exersice_2.py
@@ -1,14 +1,12 @@
 import random 
 
 def get_numbers_ticket(min, max, quantity):
-    lottery_num = set()
-    while len(lottery_num) != quantity:
-        if min >= 1 and max <= 1000:
-            lottery_num.add(random.randint(min, max))
-            return sorted(list(lottery_num))
-    return list(lottery_num)
+        if not (1 <= min <= max <= 1000 and quantity <= (max - min + 1)):
+        return []
+
+    return sorted(lottery_num = random.sample(range(min, max), quantity))
 
 
-    
+lottery_numbers = get_numbers_ticket(2, 2000, 9)
+print("Ваші лотерейні числа:", lottery_numbers)
 
-print(get_numbers_ticket(2, 110, 9))
