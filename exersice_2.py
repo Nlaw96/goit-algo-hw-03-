5b8884360fa0fff618ab2900d6f35b92238e0956
ivan perkov <ivan1996perkov@gmail.com>
2025-02-03 15:02:02 +0200

    finishing homework

--- exersice_2.py
+++ exersice_2.py
@@ -1,10 +1,10 @@
 import random 
 
-def get_numbers_ticket(min, max, quantity):
-        if not (1 <= min <= max <= 1000 and quantity <= (max - min + 1)):
+def get_numbers_ticket(min, max, quantity):    
+    if not (1 <= min <= max <= 1000 and quantity <= (max - min + 1)):
         return []
-
-    return sorted(lottery_num = random.sample(range(min, max), quantity))
+    lottery_num = random.sample(range(min, max + 1), quantity)
+    return sorted(lottery_num)
 
 
 lottery_numbers = get_numbers_ticket(2, 2000, 9)
